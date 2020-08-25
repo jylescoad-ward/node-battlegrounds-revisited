42202d4e2fe29bff08fb4b9ab2d432ddbd46b253
Jyles Coad-Ward <jyles@dariox.club>
2020-08-25 12:42:13 +0800

    changed api url

--- classes/api.js
+++ classes/api.js
@@ -20,7 +20,7 @@ class API {
   }
 
   async _req(url) {
-    const baseURL = 'http://api.dariox.club/tournament/shards'
+    const baseURL = 'http://api.dxcdn.net/tournament/shards'
     const headers = {
       'Authorization': `Bearer ${this.apikey}`,
       'Accept': 'application/vnd.api+json'
